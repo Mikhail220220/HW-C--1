44ccd0ad1c0b166eaeb767f28d64040c779ab0bb
Mikhail <M_I_X_A_I_L_1992@mail.ru>
2023-06-19 11:14:29 +0600

    finish

---
+++ task4/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    //private const int V = 2;
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        //int i = 1;
+
+        for (int i = 1; i < number; i++)
+        {
+            if (i % 2 != 0)
+                continue;
+            Console.WriteLine(i);
+        }
+    }
+}
